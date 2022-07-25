7e11d85064f1cc0ce6fb67cb2a72f5235cf2cc00
zmk5 <zkakish@gmail.com>
2022-07-25 00:11:05 -0600

    syntax and organizational fixes for deploy Dockerfile

--- deploy/pupper_entrypoint.sh
+++ deploy/pupper_entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Create a tmux config
+touch /.tmux.conf
+echo "set-option -g default-command bash" >> /.tmux.conf
+echo 'set -g default-terminal "screen-256color"' >> /.tmux.conf
+
 # Source ROS Noetic, Carthography, and Mini Pupper Workspaces
 . /opt/ros/noetic/setup.bash
 . /workspace/carto_ws/install_isolated/setup.bash
